3e0cb8a86aebca93ed8bb5f493a1e6d81a87adb4
Rizwan hameed <118429382+rizwan-signon@users.noreply.github.com>
2024-06-29 18:33:01 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutMe from "./components/AboutMe";
 import Home from "./components/Home";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Collaboration from "./components/Collaboration";
 import Navbar from "./components/NavBar";
 
